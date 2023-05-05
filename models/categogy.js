896efd2fbbc39dd9efd7d02d95eeae1bd7c6b1da
Nguyễn Ngọc Truyện <truyen027@gmail.com>
2023-05-05 22:15:51 +0700

    khởi tạo dữ án, thêm frontend

---
+++ models/categogy.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+const categogySchema = mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    description: String,
+    photo: String
+})
+
+const categogy = mongoose.model('categogy', categogySchema);
+
+module.exports = categogy
